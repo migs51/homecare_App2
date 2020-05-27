b3fe6435b51455b79d4c99f8dcde0343c1a9ce0d
Migs <mmejares@hotmail.com>
2020-05-27 01:18:49 -0500

    created react app. added layout and auth components

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import Navbar from './components/layout/Navbar';
+// import Landing from './components/layout/Landing';
+// import Register from './components/auth/Register';
+// import Login from './components/auth/Login';
+// import Alert from './components/layout/Alert';
+
+function App() {
+  return (
+    <div>
+      <h1>App</h1>
+    </div>
+  );
+}
+
+export default App;
