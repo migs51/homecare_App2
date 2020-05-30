d68023d355a3c24fb8af169d5d2b2188effe9fb6
Migs <mmejares@hotmail.com>
2020-05-30 14:01:20 -0500

    updated components

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,36 @@
-import React from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Patients from './pages/AllPatients';
-import Patient from './pages/PatientForm';
-import Nav from './components/Nav';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Routes from './components/routing/Routes';
 
-function App() {
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+
+import './App.css';
+
+const App = () => {
+  useEffect(() => {
+    setAuthToken(localStorage.token);
+    store.dispatch(loadUser());
+  }, []);
+
   return (
-    <Router>
-      <div>
-        <Nav />
-        <Switch>
-          <Route exact path={['/', '/patients']}>
-            <Patients />
-          </Route>
-          <Route exact path='/patients/:id'>
-            <Patient />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={Landing} />
+            <Route component={Routes} />
+          </Switch>
+        </Fragment>
+      </Router>
+    </Provider>
   );
-}
+};
 
 export default App;
