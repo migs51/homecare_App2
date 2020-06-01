be297edc53ef7a160ea70e2c74a6d0e11ab79e37
Migs <mmejares@hotmail.com>
2020-06-01 03:10:46 -0500

    updated patient display

--- client/src/components/profile/Patient.js
+++ client/src/components/profile/Patient.js
@@ -5,8 +5,6 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
-import ProfileExperience from './ProfileExperience';
-import ProfileEducation from './ProfileEducation';
 import { getProfileById, deleteAccount } from '../../actions/profile';
 
 const Patient = ({
@@ -44,10 +42,10 @@ const Patient = ({
             Delete Patient
           </Link>
 
-          {/* <div className='profile-grid my-1'>
+          <div className='profile-grid my-1'>
             <ProfileTop profile={profile} />
             <ProfileAbout profile={profile} />
-            <div className='profile-exp bg-white p-2'>
+            {/* <div className='profile-exp bg-white p-2'>
               <h2 className='text-primary'>Experience</h2>
               {profile.experience.length > 0 ? (
                 <Fragment>
@@ -61,9 +59,9 @@ const Patient = ({
               ) : (
                 <h4>No experience credentials</h4>
               )}
-            </div> */}
+            </div>
 
-          {/* <div className='profile-edu bg-white p-2'>
+            <div className='profile-edu bg-white p-2'>
               <h2 className='text-primary'>Education</h2>
               {profile.education.length > 0 ? (
                 <Fragment>
@@ -78,11 +76,7 @@ const Patient = ({
                 <h4>No education credentials</h4>
               )}
             </div> */}
-
-          {/* {profile.githubusername && (
-              <ProfileGithub username={profile.githubusername} />
-            )}
-          </div> */}
+          </div>
         </Fragment>
       )}
     </Fragment>
--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -1,34 +1,73 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
+import Moment from 'react-moment';
 
 const ProfileAbout = ({
   profile: {
-    bio,
-    skills,
-    user: { name }
-  }
-}) => (
-  <div className='profile-about bg-light p-2'>
-    {bio && (
-      <Fragment>
-        <h2 className='text-primary'>{name.trim().split(' ')[0]}s Bio</h2>
-        <p>{bio}</p>
-        <div className='line' />
-      </Fragment>
-    )}
-    <h2 className='text-primary'>Skill Set</h2>
-    <div className='skills'>
-      {skills.map((skill, index) => (
-        <div key={index} className='p-1'>
-          <i className='fas fa-check' /> {skill}
-        </div>
-      ))}
+    dateOfBirth,
+    gender,
+    address,
+    city,
+    state,
+    zip,
+    firstName2,
+    lastName2,
+    relationship,
+    phone,
+    address2,
+    city2,
+    state2,
+    zip2,
+  },
+}) => {
+  return (
+    <div>
+      <div className='profile-about bg-light p-2'>
+        <h3 className='text-primary'>
+          Date of Birth: <Moment format='MM/DD/YYYY' date={dateOfBirth} />
+          <br></br>
+          Age:{' '}
+          <Moment parse='YYYY-MM-DD HH:mm' fromNow ago>
+            {dateOfBirth}
+          </Moment>
+          <br></br>
+          Gender: {gender}
+        </h3>
+      </div>
+      <div className='profile-about bg-light p-2'>
+        <h3 className='text-primary'>
+          Address: {address}
+          <br></br>
+          City:{city}
+          <br></br>
+          State: {state}
+          <br></br>
+          Zip: {zip}
+        </h3>
+      </div>
+      <div className='profile-about bg-light p-2'>
+        <h3 className='text-primary'>
+          Responsible Party: {firstName2} {lastName2}
+          <br></br>
+          Relationship:{relationship}
+          <br></br>
+          Phone: {phone}
+          <br></br>
+          Address: {address2}
+          <br></br>
+          City: {city2}
+          <br></br>
+          State: {state2}
+          <br></br>
+          Zip: {zip2}
+        </h3>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 ProfileAbout.propTypes = {
-  profile: PropTypes.object.isRequired
+  profile: PropTypes.object.isRequired,
 };
 
 export default ProfileAbout;
